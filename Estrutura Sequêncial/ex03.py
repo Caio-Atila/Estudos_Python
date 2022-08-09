d65603315faddbc18c22283e379a2f2ca054a50e
Caio Átila de A. Pontes <caioatila15@gmail.com>
2022-08-09 00:38:29 -0300

    Soma de valores

---
+++ Estrutura Sequêncial/ex03.py
@@ -0,0 +1,7 @@
+#Faça um programa que peça dois números e imprima a soma.
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+soma = n1 + n2
+
+print(f'A soma entre estes valores é: {soma}')
