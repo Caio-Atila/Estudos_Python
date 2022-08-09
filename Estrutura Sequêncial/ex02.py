5fbb07960a511e03b3f13c5dd7cddc3805fc1da8
Caio Átila de A. Pontes <caioatila15@gmail.com>
2022-08-09 00:28:30 -0300

    informando um valor

---
+++ Estrutura Sequêncial/ex02.py
@@ -0,0 +1,4 @@
+#Faça um programa que peça um número e então mostre a mensagem: O número informado foi <número>.
+
+número = int(input('Informe um número: '))
+print(f'O número informado foi {número}')
