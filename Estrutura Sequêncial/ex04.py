bc27626e53a70fd0bd4467b81b026172be550f62
Caio Átila de A. Pontes <caioatila15@gmail.com>
2022-08-09 00:43:00 -0300

    Média de notas

---
+++ Estrutura Sequêncial/ex04.py
@@ -0,0 +1,9 @@
+#Faça um programa que peça as 4 notas bimestrais e mostre a média.
+
+nota1 = float(input('Informe a 1º nota: '))
+nota2 = float(input('Informe a 2º nota: '))
+nota3 = float(input('Informe a 3º nota: '))
+nota4 = float(input('Informe a 4º nota: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f'A média das notas é: {media}')
