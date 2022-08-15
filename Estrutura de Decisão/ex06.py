d7dd0486e5909277d8be5afe977057fefd5396e6
Caio Átila de A. Pontes <caioatila15@gmail.com>
2022-08-15 13:21:38 -0300

    Maior valor

---
+++ Estrutura de Decisão/ex06.py
@@ -0,0 +1,12 @@
+'''Faça um Programa que leia três números e mostre o maior deles.'''
+
+valor1 = int(input('Digite o 1º número: '))
+valor2 = int(input('Digite o 2º número: '))
+valor3 = int(input('Digite o 3º número: '))
+
+if valor3 < valor1 > valor2:
+    print(f'{valor1} é o maior valor')
+elif valor3 < valor2 > valor1:
+    print(f'{valor2} é o maior valor')
+else:
+    print(f'{valor3} é o maior valor')
