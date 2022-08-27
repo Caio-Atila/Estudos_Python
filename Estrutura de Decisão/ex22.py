3a1c045d96c53c715aa854cec6e7a417970498e5
Caio Átila de A. Pontes <caioatila15@gmail.com>
2022-08-26 23:33:01 -0300

    Par ou impar

---
+++ Estrutura de Decisão/ex22.py
@@ -0,0 +1,8 @@
+'''Faça um Programa que peça um número inteiro e determine se ele é par ou impar.'''
+
+num = int(input('Digite um valor: '))
+
+if num % 2 == 0:
+    print(f'{num} é PAR')
+else:
+    print(f'{num} é IMPAR')
