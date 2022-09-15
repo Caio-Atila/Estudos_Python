629a1140c28faae1d3ca70479876e50b84e96a97
Caio Átila de A. Pontes <caioatila15@gmail.com>
2022-09-15 11:08:57 -0300

    Func somar 3 valores

---
+++ Exercícios de funções/ex03.py
@@ -0,0 +1,5 @@
+'''Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.'''
+
+from modulos.function import somar_tres
+
+print(somar_tres())
