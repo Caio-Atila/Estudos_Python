10e89e75b56d4a0890b3030129f4e73de45984c7
Caio Átila de A. Pontes <caioatila15@gmail.com>
2022-09-15 11:10:18 -0300

    Func positivo ou negativo

---
+++ Exercícios de funções/ex04.py
@@ -0,0 +1,5 @@
+'''Faça um programa, com uma função que necessite de um argumento. A função retorna o valor de caractere "P", se seu argumento for positivo, e "N", se seu argumento for zero ou negativo.'''
+
+from modulos.function import positivo_ou_negativo
+
+print(positivo_ou_negativo())
