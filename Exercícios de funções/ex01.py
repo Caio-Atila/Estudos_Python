deb0bc0cbe7965bd41b5ab51ac61bab75e710dcd
Caio Átila de A. Pontes <caioatila15@gmail.com>
2022-09-15 11:08:31 -0300

    Func repetição de valores

---
+++ Exercícios de funções/ex01.py
@@ -0,0 +1,6 @@
+'''Faça um programa para imprimir:
+    para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.'''
+
+from modulos.function import repetir_numero
+
+repetir_numero(4, 12)
